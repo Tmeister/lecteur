a394f89631ba31adbada9483962ce04a1b029cd4
Enrique Chavez <noone@tmeister.net>
2014-08-22 15:00:37 -0500

    Less Compiled ready

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,29 @@ var gulp = require('gulp'),
 	server = lr();
 
 // Styles
-gulp.task('styles', function() {
+gulp.task('less', function() {
   return gulp.src('less/style.less')
   	.pipe(plugins.less())
 	.pipe(plugins.livereload(server))
 	.pipe(gulp.dest('./'))
-	.pipe(plugins.notify({ message: 'Styles task complete' }));
+	.pipe(plugins.notify({ message: 'Less task complete' }));
 });
 
 // Watch
 gulp.task('watch', function() {
 
   // Listen on port 35729
-  server.listen(35729, function (err) {
+  server.listen(35728, function (err) {
 	if (err) {
 	  return console.log(err)
 	};
 
 	// Watch .scss files
-	gulp.watch('less/**/*.less', ['styles']);
+	gulp.watch('less/**/*.less', ['less']);
 
   });
 
 });
 
 // Default task
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['less', 'watch']);
